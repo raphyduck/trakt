67b0e0aba6659775d984231eba7371fe2748f63d
R <raphy@hobbitton.at>
2017-02-01 11:56:36 +0000

    minor commit

--- lib/trakt/sync.rb
+++ lib/trakt/sync.rb
@@ -13,9 +13,7 @@ module Trakt
     def add_or_remove_item(action, list_type, type, list, list_name = '')
       validate_item(type, list)
       require_settings %w|client_id client_secret account_id|
-      options = {
-          type => list
-      }
+      options = list.is_a?(Array) ? {type => list} : list
       case list_type
         when 'collection'
           post("/sync/collection#{'/remove' if action == 'remove'}", options)
@@ -25,7 +23,7 @@ module Trakt
     end
 
     def validate_item(type, list)
-      raise "Invalid type #{type}" unless ['movies', 'shows', 'episodes'].include?(type)
+      raise "Invalid type #{type}" unless ['movies', 'shows', 'episodes', ''].include?(type)
       raise "Invalid type of list. 'list' must be an Array" unless list.is_a?(Array)
     end
   end
