2298e259933474f085c796ea48450aef2354a0b9
Raphy <raphyduck@users.noreply.github.com>
2025-12-07 19:39:17 +0000

    Adjust calendar auth requirements

--- lib/trakt/calendar.rb
+++ lib/trakt/calendar.rb
@@ -2,10 +2,23 @@ module Trakt
   class Calendar
     include Connection
     def premieres(*args)
-      get_with_args('/calendar/premieres.json/', *args)
+      calendar_get('/calendar/premieres.json/', *args)
     end
     def shows(*args)
-      get_with_args('/calendar/shows.json/', *args)
+      calendar_get('/calendar/shows.json/', *args)
+    end
+
+    private
+
+    def calendar_get(path, *args)
+      prepare_connection
+      require_settings %w|account_id| if calendar_requires_auth?(args)
+      arg_path = args.compact.map { |t| t.to_s }
+      get(clean_path(path), File.join(arg_path))
+    end
+
+    def calendar_requires_auth?(args)
+      args.first.to_s == 'my'
     end
   end
 end
