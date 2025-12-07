b38b32da0f963bab87d1e99c0fb0c109aa237203
Raphy <raphyduck@users.noreply.github.com>
2025-12-07 19:38:50 +0000

    Expand calendar endpoints

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,29 +1,31 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Trakt do
-  describe Trakt::Calendar do
-    let(:trakt) do
-      details = get_account_details
-      trakt          = Trakt.new :apikey => details['apikey'],
-        :username => details['username'],
-        :password => details['password']
-      trakt
-    end
-    context "premieres" do
-      it "should get premieres" do
-        result = record(self) do
-          trakt.calendar.premieres(20110421, 1)
-        end
-        result.first['episodes'].first['show']['title'].should == "24 Hour Restaurant Battle"
+describe Trakt::Calendar do
+  subject(:calendar) { described_class.new(trakt) }
+
+  let(:trakt) { Trakt::Trakt.new(account_id: 'account', client_id: 'client') }
+
+  before do
+    allow(calendar).to receive(:get_with_args).and_return([])
+  end
+
+  described_class::ROUTES.each do |method_name, (scope, segment)|
+    describe "##{method_name}" do
+      it "requests /calendars/#{scope}/#{segment}" do
+        calendar.send(method_name, '2024-01-01', 3)
+
+        expect(calendar).to have_received(:get_with_args).
+          with("/calendars/#{scope}/#{segment}", '2024-01-01', 3)
       end
     end
-    context "shows" do
-      it "should get shows" do
-        result = record(self) do
-          trakt.calendar.shows(20110416, 1)
-        end
-        result.first['episodes'].first['show']['title'].should == "Gosick"
-      end
+  end
+
+  describe '#my_movies' do
+    it 'omits missing optional arguments' do
+      calendar.my_movies('2024-02-02')
+
+      expect(calendar).to have_received(:get_with_args).
+        with('/calendars/my/movies', '2024-02-02')
     end
   end
 end
